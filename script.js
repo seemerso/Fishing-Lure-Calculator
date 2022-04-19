f52b08064b90d0255ef69a7ffb0415605ec6b3bc
seemerso <97478850+seemerso@users.noreply.github.com>
2022-04-19 19:47:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,123 +27,125 @@
     // sunblock : ['sunblock']
   };
   // what to wear items and requirements
-  const items = {
-    black: {
-      title: 'Colors : Blacks',
-      desc: 'Soft plastics: Black and Black n Blue<br>Hard Plastics : Silver, Gold, or Black',
-      requirements: data => {return data.feel == 'w';}
+  const items    = {
+    black : {
+      title        : 'Colors : Blacks',
+      desc         : 'Soft plastics: Black and Black n Blue<br>Hard Plastics : Silver, Gold, or Black',
+      requirements : (data) => {return data.feel == 'w'},
       //image        : image.src = `https://bestbassfishinglures.com/wp-content/uploads/2018/01/bass-jigs.jpg`
     },
-    dark: {
-      title: 'Colors : Darks',
-      desc: 'Soft plastics: Darker greens w/ colored flakes<br>Hard Plastics: Silver or Gold',
-      requirements: data => {return data.feel == 'ib';} },
-
-    nat: {
-      title: 'Colors : Naturals',
-      desc: 'Soft plastics: Green, Brown, White, Pinks, or Chartreuse<br>Hard Plastics: Red, White, Natrural, or Translucent',
-      requirements: data => {return data.feel == 'c';} },
-
-
-    lipless_crankbait: {
-      title: 'Lipless Crankbait',
-      desc: 'For covering shallow water quickly, there isn’t much better than a lipless crankbait. They combine vibration, noise, and speed in a way that triggers arm-rocking strikes. Lipless cranks are effective anytime bass are actively feeding and excel around shallow grass, in spawning areas, and whenever fish are schooling on shad.',
-      requirements: data => {return ['m', 'ff'].indexOf(data.gender) > -1 && data.wind != 'hw' && data.time != 'dawn' && data.time != 'dusk' && data.intensity != 'l';} },
-
-    squarebill: {
-      title: 'Squarebill Crankbait',
-      desc: 'Historically more of a niche bait of the Carolinas and Florida, in the last 15 years the square bill has been without a doubt the fastest growing segment of the hard bait industry. That’s because they catch big bass. Square bills are the spinnerbaits of rip rap. The bill shape allows them to crawl over rocks effectively and deflect in a way that the big ones can’t resist.',
-      requirements: data => {return ['m', 'ff'].indexOf(data.gender) > -1 && data.intensity != 'l' && data.time != 'dawn' && data.time != 'dusk';}
+    dark : {
+      title        : 'Colors : Darks',
+      desc         : 'Soft plastics: Darker greens w/ colored flakes<br>Hard Plastics: Silver or Gold',
+      requirements : (data) => {return data.feel == 'ib'},
+    },
+      nat : {
+      title        : 'Colors : Naturals',
+      desc         : 'Soft plastics: Green, Brown, White, Pinks, or Chartreuse<br>Hard Plastics: Red, White, Natrural, or Translucent',
+      requirements : (data) => {return data.feel == 'c'},
+    },
+    
+    lipless_crankbait : {
+      title        : 'Lipless Crankbait',
+      desc         : 'For covering shallow water quickly, there isn’t much better than a lipless crankbait. They combine vibration, noise, and speed in a way that triggers arm-rocking strikes. Lipless cranks are effective anytime bass are actively feeding and excel around shallow grass, in spawning areas, and whenever fish are schooling on shad.',
+      requirements : (data) => {return ['m','ff'].indexOf(data.gender) > -1 && data.wind != 'hw' && data.time != 'dawn' && data.time != 'dusk' && data.intensity != 'l'},
+    },
+    squarebill : {
+      title        : 'Squarebill Crankbait',
+      desc         : 'Historically more of a niche bait of the Carolinas and Florida, in the last 15 years the square bill has been without a doubt the fastest growing segment of the hard bait industry. That’s because they catch big bass. Square bills are the spinnerbaits of rip rap. The bill shape allows them to crawl over rocks effectively and deflect in a way that the big ones can’t resist.',
+      requirements : (data) => {return ['m','ff'].indexOf(data.gender) > -1 && data.intensity != 'l' && data.time != 'dawn' && data.time != 'dusk'},
       //requirements : (data) => {return data.conditions != 'r'},
     },
-    jerkbait: {
-
-      title: 'Jerkbait',
-      desc: 'Jerkbaits are shaped like minnows, look like minnows, and act like wounded minnows. Bass like to eat wounded minnows; therefore jerkbaits catch lots of bass. As simple as that sounds, there’s really not much more to it than that. Jerkbaits shine especially well in cold water, and will catch more bass than almost any other presentation when the mercury drops.',
-      requirements: data => {return ['m', 'f'].indexOf(data.gender) > -1 && data.wind != 'hw' && data.time != 'dawn' && data.time != 'dusk' && data.intensity != 'l';}
+    jerkbait : {
+   
+      title        : 'Jerkbait',
+      desc         : 'Jerkbaits are shaped like minnows, look like minnows, and act like wounded minnows. Bass like to eat wounded minnows; therefore jerkbaits catch lots of bass. As simple as that sounds, there’s really not much more to it than that. Jerkbaits shine especially well in cold water, and will catch more bass than almost any other presentation when the mercury drops.',
+      requirements : (data) => {return ['m','f'].indexOf(data.gender) > -1 && data.wind != 'hw' && data.time != 'dawn' && data.time != 'dusk' && data.intensity != 'l'},
       //url          : 'https://www.runnersworld.com/content/hats/'
     },
-    texas_rig: {
-      title: 'Texas Rig',
-      desc: 'Wether you use a senko, trick worm, craw, creature bait, fish it weighted or unweighted, the Texas Rig is the most versatile bait a bass fisherman can use.'
+    texas_rig : {
+      title        : 'Texas Rig',
+      desc         : 'Wether you use a senko, trick worm, craw, creature bait, fish it weighted or unweighted, the Texas Rig is the most versatile bait a bass fisherman can use.',
       //requirements : (data) => {return data.conditions != 'r'},
       //url          : 'https://www.runnersworld.com/content/hats/'
     },
-    jig: {
-      title: 'Jig',
-      desc: 'Flip them, cast them, drag them, hop them, swim them – it all works. They catch bass in 40 degree water and 90 degree water, in grass, rocks, wood, and open water. You can fish them in 2 feet deep or drag the bottom in 40.'
+    jig : {
+      title        : 'Jig',
+      desc         : 'Flip them, cast them, drag them, hop them, swim them – it all works. They catch bass in 40 degree water and 90 degree water, in grass, rocks, wood, and open water. You can fish them in 2 feet deep or drag the bottom in 40.',
+      //requirements : (data) => {return data.conditions != 'r'},
+      
+    },
+    crankbait : {
+      title        : 'Crankbait',
+      desc         : 'If it wasn’t for jigs, crankbaits would probably hold the crown for best “year-round bait”. By changing your crankbaits with the seasons and water conditions, you can always find bass willing to bite.',
       //requirements : (data) => {return data.conditions != 'r'},
     },
-
-    crankbait: {
-      title: 'Crankbait',
-      desc: 'If it wasn’t for jigs, crankbaits would probably hold the crown for best “year-round bait”. By changing your crankbaits with the seasons and water conditions, you can always find bass willing to bite.'
+    swimbait : {
+      title        : 'Swimbait',
+      desc         : 'Despite being a relative newcomer in the bass tackle industry, swimbaits have already earned their place as big-time fish catchers. They can emulate a variety of baitfish species and as such are effective all 12 months of the year. In winter, anglers will often hop them on an underspin or umbrella rig and let them fall to emulate a dying shad. In the summer, swimbaits are as effective on a jighead in open water as they are on a weedless hook through shallow grass.',
+      requirements : (data) => {return data.conditions != 'c'},
+      //url          : 'https://www.runnersworld.com/content/hats/'
+    },
+    drop_shot : {
+      title        : 'Drop Shot',
+      desc         : 'The drop shot rig is one of the most effective worm rigs for anglers battling a tough bite. It’s so effective that it has become the go-to technique for tournament anglers that need to put bass in the livewell.',
+      requirements : (data) => {return ['f'].indexOf(data.gender) > -1 && data.intensity != 'l'},
+      //url          : 'https://www.runnersworld.com/content/hats/'
+    },
+    spinnerbait : {
+      title        : 'Spinnerbait',
+      desc         : 'No bass fishing list would be complete without the venerable spinnerbait. There’s a good reason for that too – they still catch the snot out of them. Around flooded brush, laydowns, and wind gusts, there isn’t much better than a spinnerbait. They come through cover excellently and bass can’t resist their combination of flash and thump.',
       //requirements : (data) => {return data.conditions != 'r'},
-    },
-    swimbait: {
-      title: 'Swimbait',
-      desc: 'Despite being a relative newcomer in the bass tackle industry, swimbaits have already earned their place as big-time fish catchers. They can emulate a variety of baitfish species and as such are effective all 12 months of the year. In winter, anglers will often hop them on an underspin or umbrella rig and let them fall to emulate a dying shad. In the summer, swimbaits are as effective on a jighead in open water as they are on a weedless hook through shallow grass.',
-      requirements: data => {return data.conditions != 'c';}
+      requirements : (data) => {return ['pc','o'].indexOf(data.conditions) > -1 || data.wind == 'hw'},
       //url          : 'https://www.runnersworld.com/content/hats/'
     },
-    drop_shot: {
-      title: 'Drop Shot',
-      desc: 'The drop shot rig is one of the most effective worm rigs for anglers battling a tough bite. It’s so effective that it has become the go-to technique for tournament anglers that need to put bass in the livewell.',
-      requirements: data => {return ['f'].indexOf(data.gender) > -1 && data.intensity != 'l';}
+    chatterbait : {
+      title        : 'Chatterbait',
+      desc         : 'Keep in mind that bladed swim jigs or vibrating jigs are most effective when bass are shallow and active. However, there are some exceptions to this rule, but savvy and experienced bass anglers can find many other good uses for vibrating jigs throughout the year.',
+      requirements : (data) => {return ['pc','o'].indexOf(data.conditions) > -1 || data.wind == 'hw'},
       //url          : 'https://www.runnersworld.com/content/hats/'
     },
-    spinnerbait: {
-      title: 'Spinnerbait',
-      desc: 'No bass fishing list would be complete without the venerable spinnerbait. There’s a good reason for that too – they still catch the snot out of them. Around flooded brush, laydowns, and wind gusts, there isn’t much better than a spinnerbait. They come through cover excellently and bass can’t resist their combination of flash and thump.',
-      //requirements : (data) => {return data.conditions != 'r'},
-      requirements: data => {return ['pc', 'o'].indexOf(data.conditions) > -1 || data.wind == 'hw';}
-      //url          : 'https://www.runnersworld.com/content/hats/'
-    },
-    chatterbait: {
-      title: 'Chatterbait',
-      desc: 'Keep in mind that bladed swim jigs or vibrating jigs are most effective when bass are shallow and active. However, there are some exceptions to this rule, but savvy and experienced bass anglers can find many other good uses for vibrating jigs throughout the year.',
-      requirements: data => {return ['pc', 'o'].indexOf(data.conditions) > -1 || data.wind == 'hw';}
-      //url          : 'https://www.runnersworld.com/content/hats/'
-    },
-    buzzbait: {
-      title: 'Buzzbait',
-      desc: 'Fishing a buzzbait just might be the most exciting style of bass fishing there is. They are one of the best topwater lures you can tie on, and any bass fisherman will agree that there is nothing like seeing a bass explode on a topwater lure.',
-      requirements: data => {return ['dawn', 'dusk'].indexOf(data.time) > -1 || data.gender == 'f';}
+    buzzbait : {
+      title        : 'Buzzbait',
+      desc         : 'Fishing a buzzbait just might be the most exciting style of bass fishing there is. They are one of the best topwater lures you can tie on, and any bass fisherman will agree that there is nothing like seeing a bass explode on a topwater lure.',
+      requirements : (data) => {return ['dawn','dusk'].indexOf(data.time) > -1 || data.gender == 'f'},
       //requirements : (data) => {return ['dawn','dusk'].indexOf(data.time) > -1 || data.gender == 'f'},
       //url          : 'https://www.runnersworld.com/content/hats/'
     },
-    wacky_rig: {
-      title: 'Wacky Rig',
-      desc: 'The wacky rig is a very popular way to rig a rubber worm. This is when the hook is centered on the body of the worm, causing the to sag evenly on both sides. Sometimes anglers will prefer the hook to be off-center but most of the time it’s centered.',
-      requirements: data => {return data.wind != 'hw' && data.intensity != 'l';}
+    wacky_rig : {
+      title        : 'Wacky Rig',
+      desc         : 'The wacky rig is a very popular way to rig a rubber worm. This is when the hook is centered on the body of the worm, causing the to sag evenly on both sides. Sometimes anglers will prefer the hook to be off-center but most of the time it’s centered.',
+      requirements : (data) => {return data.wind != 'hw' && data.intensity != 'l'},
       //requirements : (data) => {return data.conditions != 'r'},
       //url          : 'https://www.runnersworld.com/content/hats/'
     },
-    ned_rig: {
-      title: 'Ned Rig',
-      desc: 'The Ned rig with Ned rig soft plastics has quickly become unstoppable in its unrelenting dominance of the light line finesse fishing soft stickbait category. It began as a rather niche freshwater finesse technique in the Midwest, mainly for smallmouth bass. However, it has now exploded into its own unique soft plastic sub-category. ',
-      requirements: data => {return data.wind != 'hw' && data.intensity != 'l';}
+    ned_rig : {
+      title        : 'Ned Rig',
+      desc         : 'The Ned rig with Ned rig soft plastics has quickly become unstoppable in its unrelenting dominance of the light line finesse fishing soft stickbait category. It began as a rather niche freshwater finesse technique in the Midwest, mainly for smallmouth bass. However, it has now exploded into its own unique soft plastic sub-category. ',
+      requirements : (data) => {return data.wind != 'hw' && data.intensity != 'l'},
       //url          : 'https://www.runnersworld.com/content/hats/'
     },
-    frog: {
-      title: 'Frog',
-      desc: 'If you fish lakes with any kind of vegetation, a frog may be the only bait you ever need. They are totally weedless, allowing anglers to access fish that are otherwise uncatchable. Whether worked over massive milfoil flats, skipped underneath hanging cover, or worked through lily pad fields, frogs are the deal for any vegetation situation.',
+    frog : {
+      title        : 'Frog',
+      desc         : 'If you fish lakes with any kind of vegetation, a frog may be the only bait you ever need. They are totally weedless, allowing anglers to access fish that are otherwise uncatchable. Whether worked over massive milfoil flats, skipped underneath hanging cover, or worked through lily pad fields, frogs are the deal for any vegetation situation.',
       //requirements : (data) => {return (data.gender == 'f') || (data.time == 'dawn','dusk')},
-      requirements: data => {return ['dawn', 'dusk'].indexOf(data.time) > -1 || data.gender == 'f';}
+      requirements : (data) => {return ['dawn','dusk'].indexOf(data.time) > -1 || data.gender == 'f'},
       //requirements : (data) => {return data.time == 'dawn','dusk'},
       //url          : 'https://www.runnersworld.com/content/sunglasses/'
     },
-    popper: {
-      title: 'Popper',
-      desc: 'These lures are designed to ripple the water’s surface, making popping and splashing sounds as they are retrieved. These baits are designed to create the appearance of prey in distress, a sound that is hard for a bass to resist.',
-      requirements: data => {return ['dawn', 'dusk'].indexOf(data.time) > -1 || data.gender == 'f';}
-      // url          : 'https://www.runnersworld.com/content/jackets/'
+    popper : {
+      title        : 'Popper',
+      desc         : 'These lures are designed to ripple the water’s surface, making popping and splashing sounds as they are retrieved. These baits are designed to create the appearance of prey in distress, a sound that is hard for a bass to resist.',
+      requirements : (data) => {return ['dawn','dusk'].indexOf(data.time) > -1 || data.gender == 'f'},
+     // url          : 'https://www.runnersworld.com/content/jackets/'
     },
-    whopper_plopper: {
-      title: 'Whopper Plopper',
-      desc: 'Whopper ploppers are one of the easiest lures to fish. One, the most effective way to fish them is simply reeling them in a straight retrieve the way you would fish any prop bait. The motion of the bait gliding across the surface is what creates the tail to spin and make the “plopping” noise.',
-      // url          : 'https://www.runnersworld.com/content/jackets/'
-      requirements: data => {return ['dawn', 'dusk'].indexOf(data.time) > -1 || data.intensity == 'l';} } };
+    whopper_plopper : {
+      title        : 'Whopper Plopper',
+      desc         : 'Whopper ploppers are one of the easiest lures to fish. One, the most effective way to fish them is simply reeling them in a straight retrieve the way you would fish any prop bait. The motion of the bait gliding across the surface is what creates the tail to spin and make the “plopping” noise.',
+     // url          : 'https://www.runnersworld.com/content/jackets/'
+      requirements : (data) => {return ['dawn','dusk'].indexOf(data.time) > -1 || data.intensity == 'l'},
+    }
+  };
 
 
   // text values for results text
